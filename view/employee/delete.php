20f4fc01818479f69348326f353a47e3e9f938da
samet <99058368@mydavinci.nl>
2021-10-26 12:37:20 +0200

    index & delete.php working correctly.

--- view/employee/delete.php
+++ view/employee/delete.php
@@ -1,4 +1,4 @@
 <?php
-// maak een bevestig pagina voor het verwijderen van een mededwerker
+	echo 'lol';
 ?>
 
--- view/employee/index.php
+++ view/employee/index.php
@@ -1,16 +1,16 @@
 	<?php
-		// maak een overzicht lijst van ALLE personen
+	//	echo "<pre>";
+	//print_r($employees);
+	//echo "</pre>";
 	?>
 	<h1>Overzicht van personen</h1>
 	<ul>
+		<?php foreach ( $employees as $employee ) { ?>
 		<li>
-			<span>Karel is 12 jaar</span>
-			<?php
-			// de opbouw van de link bepaald welke method in welke controller aangeroepen wordt.
-			// het woordje "employee" in de url betekent dat het framework moet zoeken naar een controller genaamd "EmployeeController".
-			// Hij maakt van de eerste letter een hoofdletter en plakt er zelf "Controller" achter.
-			// Het woordje "update" of "delete" betekent dat hij in deze controller moet zoeken naar een method met deze naam.
-			?>
-			<a href="/employee/update/1">Wijzigen</a> <a href="/employee/delete/1">Verwijderen</a>
+            <span><?php echo $employee['name'] ?> is <?php echo $employee['age'] ?> jaar</span>
+			<a href="/employee/update/<?php echo $employee['id'] ?>">Wijzigen</a> <a href="/employee/delete/<?php echo $employee['id'] ?>">Verwijderen</a>
+            <?php
+                };
+            ?>
 		</li>
 	</ul>
